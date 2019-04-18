56c90d22fe477e404e27eed789912ed07f45665c
hagerg <42995901+hagerg@users.noreply.github.com>
2019-04-18 21:37:41 +0200

    Added functionality for showing expanded information for clicked notificationListItem

--- blomsterLandet/src/screens/NotificationScreen.js
+++ blomsterLandet/src/screens/NotificationScreen.js
@@ -1,28 +1,66 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
 import { NotificationListItem } from '../components/notifications';
+import NotificationExpanded from '../components/notifications/NotificationExpanded';
 
 class NotificationScreen extends React.Component {
-    
-    state = { notificationExpanded: false, 
-        notifications: [{ title: 'Tomat', description: 'Dags att odla tomater!' }] };
 
+    state = {
+        notificationExpanded: false,
+        notifications: [{ title: 'Tomat', description: 'Dags att odla tomater!' }],
+        notificationMap: [],
+        expandedTitle: '',
+        expandedDescription: ''
+    };
+
+    componentWillMount() {
+        this.listNotifications();
+    }
+
+    //Creates a map with notificationListItems, the key needs to be reworked as it 
+    //might not always be unique
+    mapNotifications() {
+        const mapOfNotifications = (this.state.notifications.map(notification =>
+            (<NotificationListItem
+                key={notification.title}
+                title={notification.title}
+                pressed={this.openExpandedNotification.bind(this)}
+            />)
+        ));
+        this.setState({ notificationMap: mapOfNotifications });
+    }
+
+    //Returns the map with notificationListItems if it exists, 
+    //otherwise it creates it and returns it
     listNotifications() {
-        //Returns a map with notificationListItems, the key needs to be reworked as it 
-        //might not always be unique
-        return (this.state.notifications.map(notification =>
-            (<NotificationListItem title={notification.title} key={notification.title}>
-                {notification.title}
-            </NotificationListItem>)
-        )
-        );
+        if (this.state.notificationMap.length === 0) {
+            this.mapNotifications();
+        }
+        return this.state.notificationMap;
+    }
+
+    //Opens the expanded notification based on the title of the notification that has been clicked
+    //The key needs to be reworked as it doesn't have to be uniqe right now, which will cause issues
+    openExpandedNotification(title) {
+        const description = this.state.notifications[0].description;
+        this.setState({
+            notificationExpanded: true,
+            expandedTitle: title,
+            expandedDescription: description
+        });
+        console.log(this.state);
     }
 
     render() {
         return (
             <View>
+                <NotificationExpanded
+                    title={this.state.expandedTitle}
+                    description={this.state.expandedDescription}
+                    modalvisible={this.notificationExpanded}
+                />
                 <ScrollView>
-                    {this.listNotifications()} 
+                    {this.state.notificationMap}
                 </ScrollView>
             </View>
         );
