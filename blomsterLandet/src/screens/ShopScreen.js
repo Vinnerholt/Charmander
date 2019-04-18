e35bbf2ffd2290a9d6d3368a23a28ac80ea03184
Caroline Jönsson <carolinejonsson@Carolines-MacBook-Air.local>
2019-04-18 11:31:58 +0200

    buybutton added
    Co-authored-by:AronSeamountain aronsj@student.chalmers.se”
    
    :q

--- blomsterLandet/src/screens/ShopScreen.js
+++ blomsterLandet/src/screens/ShopScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import BuyButton from '../components/BuyButton';
 
 class ShopScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Shop!</Text>
+                <BuyButton title='Köp nu' />
             </View>
         );
     }
