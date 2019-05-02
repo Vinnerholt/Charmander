19aacce7e20d0eebc02d7c897335b4e48c1fe479
Vinnerholt <benno_fotboll@hotmail.com>
2019-05-02 21:08:49 +0200

    Fixed some formatting issues, fixed and removed some styles, resourced the tomato image and some more small fixes.

--- blomsterLandet/src/components/common/CollapseButton.js
+++ blomsterLandet/src/components/common/CollapseButton.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { Collapse, CollapseHeader, CollapseBody } from 'accordion-collapse-react-native';
 
-onst CollapseButton = (header, body) => {
+const CollapseButton = ({ header, body }) => {
     const {
-        bottomButtonStyle, bottomButtonTextStyle
+        collapseButtonStyle, headerTextStyle
     } = styles;
 
     return (
-        <Collapse style={bottomButtonStyle}>
+        <Collapse style={collapseButtonStyle}>
             <CollapseHeader>
                 <View>
-                    <Text style={bottomButtonTextStyle}>{header}</Text>
+                    <Text style={headerTextStyle}>{header}</Text>
                 </View>
             </CollapseHeader>
 
@@ -22,30 +23,30 @@ onst CollapseButton = (header, body) => {
 };
 
 const styles = {
-bottomButtonStyle: {
-    width: '90%',
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#3e5f36',
-    marginLeft: 5,
-    marginRight: 5,
-    justifyContent: 'center',
-    paddingLeft: 10,
-    shadowColor: '#050',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2
-},
-bottomButtonTextStyle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    alignItems: 'flex-end',
-    color: '#3e5f36',
-    paddingTop: 10,
-    paddingBottom: 10
-},
-},
+    collapseButtonStyle: {
+        width: '90%',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#3e5f36',
+        marginLeft: 5,
+        marginRight: 5,
+        justifyContent: 'center',
+        paddingLeft: 10,
+        shadowColor: '#050',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+    },
+    headerTextStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignItems: 'flex-end',
+        color: '#3e5f36',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+};
 
 
 export default CollapseButton;
