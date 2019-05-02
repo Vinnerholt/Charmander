6e22bd4e1a9db71fcbe0f51e38388b4332b405a9
ilcarolinejonsson <ilcarolinejonsson@gmail.com>
2019-05-02 16:35:15 +0200

    Fixade liten bild vid objektet i listan

--- blomsterLandet/src/components/myplants/PlantListItem.js
+++ blomsterLandet/src/components/myplants/PlantListItem.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const PlantListItem = (props) => {
     return (
         <TouchableOpacity onPress={() => props.navigation.navigate('Details')} style={styles.itemStyle}>
+            <Image 
+                source={require('../../resources/images/tomat.jpg')}
+                style={{
+                    width: '10%',
+                    height: '100%'
+            }} />
             <Text style={styles.textStyle}>
                 Hej
             </Text>
             <Icon name="chevron-right" style={styles.arrowStyle} />
-    </TouchableOpacity>
+        </TouchableOpacity>
     );
 };
 
 const styles = {
+
     textStyle: {
         flex: 9,
         color: 'black',
-        fontSize: 16
+        fontSize: 16,
+        paddingLeft: 5
     },
     arrowStyle: {
         fontSize: 30,
