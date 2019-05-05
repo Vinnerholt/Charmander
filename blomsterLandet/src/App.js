02cfac449ca23b83c182d4c9ed2584e7a108a6d1
Jonathan Köre <korejonathan@gmail.com>
2019-05-06 01:24:19 +0200

    Moved logic from app component to seperate class

--- blomsterLandet/src/App.js
+++ blomsterLandet/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import { Image, ImageBackground, Platform } from 'react-native';
-import firebase from 'react-native-firebase';
+import { Image, ImageBackground } from 'react-native';
 
 import { Header } from './components/common/index';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import * as firebaseHandler from './firebaseHandler';
 import * as notifHandler from './notifHandler';
 import NotificationScreen from './screens/NotificationScreen';
 import PlantDetailScreen from './screens/PlantDetailScreen';
@@ -52,62 +50,12 @@ const TabNavigatorContainer = createAppContainer(TabNavigator);
 
 //The main application
 class App extends React.Component {
-    componentWillMount() {
-        //Connects App to firebase.
-        //firebaseHandler.initializeApp();
-    }
     componentDidMount() {
+        //Sets up support for receiving notifications
         notifHandler.initNotifications();
-        //Build a channel
-        //Tydligen måste man bygga en kanal som notisen tas emot på, jag döpte den till test channel.
-        //Importance av kanalen måste vara high, annars visas den inte om den drop down notis, utan bara i bakgrunden.
-        const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', firebase.notifications.Android.Importance.High)
-        .setDescription('My apps test channel');
-
-        // Create the channel
-        firebase.notifications().android.createChannel(channel);
-        this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification) => {
-            // Process your notification as required
-            // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
-        });
-        this.notificationListener = firebase.notifications().onNotification((notification) => {
-            // Process your notification as required
-
-            //Hit kommer du när du väl får notisen, och så väljer den olika inställningar
-            //beroende på vilken platform du är på.
-            if (Platform.OS === 'android') {
-                const localNotification = new firebase.notifications.Notification()
-                  .setNotificationId(notification.notificationId)
-                  .setTitle(notification.title)
-                  .setSubtitle(notification.subtitle)
-                  .setBody(notification.body)
-                  .setData(notification.data)
-                  .setSound('default')
-                  .android.setChannelId('test-channel') // e.g. the id you chose above
-                  .android.setSmallIcon('ic_stat_notification') // create this icon in Android Studio
-                  .android.setColor('#000000') // you can set a color here
-                  .android.setPriority(firebase.notifications.Android.Priority.High);
-        
-                  //Här skickas notisen (till sig själv), med andra ord visas
-                firebase.notifications().displayNotification(localNotification);
-              } else if (Platform.OS === 'ios') {
-                const localNotification = new firebase.notifications.Notification()
-                  .setNotificationId(notification.notificationId)
-                  .setTitle(notification.title)
-                  .setSubtitle(notification.subtitle)
-                  .setBody(notification.body)
-                  .setData(notification.data)
-                  .ios.setBadge(notification.ios.badge);
-        
-                firebase.notifications()
-                  .displayNotification(localNotification)
-                  .catch(err => console.error(err));
-              }
-            });
     }
-    componentWillUnmount() {
-        this.notificationDisplayedListener();
-        this.notificationListener();  
+    componentWillUnmount() { 
+        notifHandler.unmountNotifListeners();
     }
     render() {
         return (
--- blomsterLandet/src/notifHandler.js
+++ blomsterLandet/src/notifHandler.js
@@ -1,6 +1,14 @@
-import firebase, { Notification } from 'react-native-firebase';
-export async function initNotifications () {
-    let enabled = await firebase.messaging().hasPermission();
+import firebase from 'react-native-firebase';
+import { Platform } from 'react-native';
+
+export function initNotifications() {
+    checkPermissions();
+    createChannel();
+    mountNotifListeners();
+}
+
+function checkPermissions() {
+    const enabled = firebase.messaging().hasPermission();
     if (enabled) {
         // user has permissions
         console.log('user has permissions');
@@ -12,22 +20,65 @@ export async function initNotifications () {
                 // User has authorised  
             })
             .catch(error => {
-                // User has rejected permissions  
+                // User has rejected permissions
+                console.log(error);  
             });
     }
 }
 
-export function mountListeners () {
-    this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((Notification) => {
-        // Process your notification as required
-        // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
-    });
-    this.notificationListener = firebase.notifications().onNotification((Notification) => {
-        // Process your notification as required
-    });
+function createChannel() {
+    //Sets up a channel where notifications can be received
+    const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', 
+        firebase.notifications.Android.Importance.High).setDescription('My apps test channel');
+
+    // Create the channel
+    firebase.notifications().android.createChannel(channel);
 }
 
-export function unmountListeners () {
+function mountNotifListeners() {
+    this.notificationDisplayedListener = firebase.notifications()
+        .onNotificationDisplayed((notification) => {
+        // Process your notification as required
+        // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+        console.log('Received');
+    });
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+        // Process your notification as required
+
+        //Hit kommer du när du väl får notisen, och så väljer den olika inställningar
+        //beroende på vilken platform du är på.
+        if (Platform.OS === 'android') {
+            const localNotification = new firebase.notifications.Notification()
+                .setNotificationId(notification.notificationId)
+                .setTitle(notification.title)
+                .setSubtitle(notification.subtitle)
+                .setBody(notification.body)
+                .setData(notification.data)
+                .setSound('default')
+                .android.setChannelId('test-channel') // e.g. the id you chose above
+                .android.setSmallIcon('ic_stat_notification') // create this icon in Android Studio
+                .android.setColor('#000000') // you can set a color here
+                .android.setPriority(firebase.notifications.Android.Priority.High);
+    
+                //Här skickas notisen (till sig själv), med andra ord visas
+            firebase.notifications().displayNotification(localNotification);
+            } else if (Platform.OS === 'ios') {
+            const localNotification = new firebase.notifications.Notification()
+                .setNotificationId(notification.notificationId)
+                .setTitle(notification.title)
+                .setSubtitle(notification.subtitle)
+                .setBody(notification.body)
+                .setData(notification.data)
+                .ios.setBadge(notification.ios.badge);
+    
+            firebase.notifications()
+                .displayNotification(localNotification)
+                .catch(err => console.error(err));
+            }
+        });
+}
+
+export function unmountNotifListeners() {
     this.notificationDisplayedListener();
     this.notificationListener();  
 }
