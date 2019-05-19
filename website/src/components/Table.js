280ec2287163385dc1a9f1e2ad805a932af782e9
hagerg <42995901+hagerg@users.noreply.github.com>
2019-05-19 15:32:17 +0200

    Fixed so that the website now works with the change in the backend, namely the products for a placed order that are stored in an array

--- website/src/components/Table.js
+++ website/src/components/Table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Text } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -7,33 +7,35 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/Paper';
+import ListItem from '@material-ui/core/Paper';
 import firebase from 'firebase';
 import * as orderReader from '../orderReader';
 
 const CustomTableCell = withStyles(theme => ({
-  head: {
-	backgroundColor: theme.palette.common.black,
-	color: theme.palette.common.white,
-  },
-  body: {
-	fontSize: 14,
-  },
+	head: {
+		backgroundColor: theme.palette.common.black,
+		color: theme.palette.common.white,
+	},
+	body: {
+		fontSize: 14,
+	},
 }))(TableCell);
 
 const styles = theme => ({
-  root: {
-	width: '100%',
-	marginTop: theme.spacing.unit * 3,
-	overflowX: 'auto',
-  },
-  table: {
-	minWidth: 700,
-  },
-  row: {
-	'&:nth-of-type(odd)': {
-	  backgroundColor: theme.palette.background.default,
+	root: {
+		width: '100%',
+		marginTop: theme.spacing.unit * 3,
+		overflowX: 'auto',
 	},
-  },
+	table: {
+		minWidth: 700,
+	},
+	row: {
+		'&:nth-of-type(odd)': {
+			backgroundColor: theme.palette.background.default,
+		},
+	},
 });
 
 
@@ -51,62 +53,92 @@ class CustomizedTable extends React.Component {
 	listenForChanges() {
 		// re-generates rows whenever something in the collection 'orders' changes.
 		firebase.firestore().collection('orders').onSnapshot(r => {
-				this.generateRows();
+			this.generateRows();
 		});
 	}
-	generateRows = async() => {
+	generateRows = async () => {
 		//Creates a unique key for each row
 		let rowKey = 0;
 		//Fetches the orders from firebase
 		let orders = await orderReader.fetchData();
 
-		for(let i in orders) {
+		for (let i in orders) {
 			orders[i].rowKey = rowKey;
 			rowKey++;
 			//Since the product in the order is a reference to a product, the product
 			//in question must be fetched from firebase aswell.
 			try {
-				const productSnapshot = await orders[i].product.get();
-				const productData = await productSnapshot.data();
-				const productName = productData.name;
-				orders[i].product = productName;
+				for (let j in orders[i].order) {
+					const productSnapshot = await orders[i].order[j].product.get();
+					const productData = await productSnapshot.data();
+					const productName = productData.name;
+					orders[i].order[j].product = productName;
+				}
 			} catch (err) {
-				
+				console.log(err);
 			}
-			
+
 		}
 		this.setState({ rows: orders });
 	}
-	
+
+	renderProductList(orders) {
+		if (orders == null)
+			return;
+		if (orders != null) {
+			if (orders.length > 0) {
+				return (
+					<List>{orders.map(order => (
+						<ListItem>{order.product}</ListItem>
+					))}
+					</List>
+				);
+			}
+		}
+	}
+
+	renderRow(row) {
+		if (row.rowKey != null) {
+			return (<TableRow className={this.props.classes.row} key={row.rowKey}>
+				<CustomTableCell align="right">
+					{this.renderProductList(row.order)}
+				</CustomTableCell>
+				<CustomTableCell align="right">{row.buyer}</CustomTableCell>
+				<CustomTableCell align="right">{row.amount}</CustomTableCell>
+			</TableRow>);
+		}
+	}
+	renderRows() {
+		if (this.state.rows != null) {
+			return this.state.rows.map(row => (
+				this.renderRow(row)
+			));
+		}
+	}
+
 	render() {
 		return (
 			<Paper className={this.props.classes.root}>
-			  <Table className={this.props.classes.table}>
-				<TableHead>
-				  <TableRow>
-					<CustomTableCell align="right">Product</CustomTableCell>
-					<CustomTableCell align="right">Buyer</CustomTableCell>
-					<CustomTableCell align="right">Amount</CustomTableCell>
-				  </TableRow>
-				</TableHead>
-				<TableBody>
-				  {this.state.rows.map(row => (
-					<TableRow className={this.props.classes.row} key={row.rowKey}>
-						<CustomTableCell align="right">{row.product}</CustomTableCell>
-					  <CustomTableCell align="right">{row.buyer}</CustomTableCell>
-					  <CustomTableCell align="right">{row.amount}</CustomTableCell>
-					</TableRow>
-				  ))}
-				</TableBody>
-			  </Table>
+				<Table className={this.props.classes.table}>
+					<TableHead>
+						<TableRow>
+							<CustomTableCell align="right">Product</CustomTableCell>
+							<CustomTableCell align="right">Buyer</CustomTableCell>
+							<CustomTableCell align="right">Amount</CustomTableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{this.renderRows()}
+					</TableBody>
+				</Table>
 			</Paper>
-		  );
+		);
 	}
-	
+
 }
 
 CustomizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(CustomizedTable);
