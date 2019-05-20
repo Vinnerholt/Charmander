da7f5a99e46b6939dfa0c5939079236e8207006d
hagerg <42995901+hagerg@users.noreply.github.com>
2019-05-20 08:37:15 +0200

    made the header-text bigger

--- website/src/components/Table.js
+++ website/src/components/Table.js
@@ -15,6 +15,7 @@ const CustomTableCell = withStyles(theme => ({
 	head: {
 		backgroundColor: theme.palette.common.black,
 		color: theme.palette.common.white,
+		fontSize: 18,
 	},
 	body: {
 		fontSize: 14,
