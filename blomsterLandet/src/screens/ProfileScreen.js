8f525be7944852deaef6807ea4022692369c80d2
AronSeamountain <aronsj@student.chalmers.se>
2019-05-24 15:01:16 +0200

    Scooby doo

--- blomsterLandet/src/screens/ProfileScreen.js
+++ blomsterLandet/src/screens/ProfileScreen.js
@@ -27,7 +27,7 @@ class ProfileScreen extends React.Component {
                         <Image
                             style={imageStyle}
                             // eslint-disable-next-line global-require
-                            source={require('../resources/images/ros.jpg')}
+                            source={require('../resources/images/scooby-doo-picture.jpeg')}
                         />
                     </View>
                 </View>
@@ -62,7 +62,7 @@ Andra relevanta saker.`}
                     </View>
                 </View>
             </ScrollView>
-    
+
         );
     }
 }
